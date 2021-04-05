85f154908521bded028d7977426098672af2a850
steven <chivy1204@gmail.com>
2021-04-06 06:58:18 +0700

    Create two node

--- src/preprocess_data/preprocess.py
+++ src/preprocess_data/preprocess.py
@@ -9,11 +9,14 @@ def _preprocess_data():
      y = np.array(data["label"])
      data.drop(columns=["label"])
      X = np.array(data)
-     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-     np.save('x_train.npy', X_train)
+     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+     X_train1, y_train1, X_train2, y_train2 = train_test_split(X, y, test_size=0.5)
+     np.save('x_train1.npy', X_train1)
      np.save('x_test.npy', X_test)
-     np.save('y_train.npy', y_train)
+     np.save('y_train1.npy', y_train1)
      np.save('y_test.npy', y_test)
+     np.save('x_train2.npy', X_train1)
+     np.save('y_train2.npy', y_train1)
      
 if __name__ == '__main__':
      print('Preprocessing data...')
--- src/train/train.py
+++ src/train/train.py
@@ -3,13 +3,13 @@ import joblib
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
-def train_model(x_train, y_train):
+def train_model(x_train, y_train, x_test, y_test):
     x_train_data = np.load(x_train)
     y_train_data = np.load(y_train)
 
     model = RandomForestClassifier(n_estimators=100,max_depth=1, random_state=42)
     model.fit(x_train_data, y_train_data)
-    
+    print(model.score(x_test, y_test))
     joblib.dump(model, 'model.pkl')
 
 
@@ -17,5 +17,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--x_train')
     parser.add_argument('--y_train')
+    parser.add_argument('--x_test')
+    parser.add_argument('--y_test')
     args = parser.parse_args()
-    train_model(args.x_train, args.y_train)
+    train_model(args.x_train, args.y_train, args.x_test, args.y_test)

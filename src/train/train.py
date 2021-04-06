a3ca9bce57217ed495db5e3fc52093d5e091d0fd
steven <chivy1204@gmail.com>
2021-04-06 08:54:56 +0700

    Change number of tree

--- src/train/train.py
+++ src/train/train.py
@@ -8,7 +8,7 @@ def train_model(x_train, y_train, x_test, y_test):
     y_train_data = np.load(y_train)
     y_test = np.load(y_test)
     x_test = np.load(x_test)
-    model = RandomForestClassifier(n_estimators=100,max_depth=1, random_state=42)
+    model = RandomForestClassifier(n_estimators=10,max_depth=1, random_state=42)
     model.fit(x_train_data, y_train_data)
     print(model.score(x_test, y_test))
     #joblib.dump(model, 'model.pkl')

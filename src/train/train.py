e81b51dffafc67febf2a6f60c25a90142424fba0
steven <chivy1204@gmail.com>
2021-04-06 09:01:55 +0700

    Change tree

--- src/train/train.py
+++ src/train/train.py
@@ -6,11 +6,13 @@ from sklearn.ensemble import RandomForestClassifier
 def train_model(x_train, y_train, x_test, y_test):
     x_train_data = np.load(x_train)
     y_train_data = np.load(y_train)
+    print("Total samples: ", y_train_data.shape[0])
+    print("Attack samples: ", np.sum(y_train_data))
     y_test = np.load(y_test)
     x_test = np.load(x_test)
     model = RandomForestClassifier(n_estimators=10,max_depth=1, random_state=42)
     model.fit(x_train_data, y_train_data)
-    print(model.score(x_test, y_test))
+    print("Accuracy: ", model.score(x_test, y_test))
     #joblib.dump(model, 'model.pkl')
 
 
